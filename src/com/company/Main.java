89291b4fa70b61867648a9945105f33eaa647dd9
YassSSH <yassine.regragui@mediaschool.me>
2022-03-26 21:46:18 +0100

    IntelliJ tuto

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        Product product = new Product("Yass");
+
+        System.out.println(product.getName());
+    }
+}
