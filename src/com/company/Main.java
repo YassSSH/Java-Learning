10ad1dacbdb5c6c22e2c57e3f9be53fabebe8b8c
YassSSH <yassine.regragui@mediaschool.me>
2022-03-27 19:59:30 +0200

    Starting Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 public class Main {
+    private static void calcul(int num) {
 
+        for (int i = 0; i< num + 1 ; i++) {
+            System.out.println("Liste : " + i);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("test");
-
-        Product product = new Product("Yass");
-
-        System.out.println(product.getName());
+        calcul(9);
     }
 }
